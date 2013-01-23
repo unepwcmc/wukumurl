a25182b4dd2e6be61271a9a5139b0620e6db62e4
James Cox <th3james@gmail.com>
2013-01-23 17:17:57 +0000

    Fix tests

--- test/unit/short_url_test.rb
+++ test/unit/short_url_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
 class ShortUrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  test "visits_today should return a count of visits today" do
-    debugger
-    assert_equal 1, short_urls(:hn).find.visits_today
+  test "visits_today should return only visits from today" do
+    assert_equal 1, short_urls(:hn).visits_today.count
+    assert_equal visits(:visit_hn_today), short_urls(:hn).visits_today[0]
+  end
+
+  test "visits_this_week should return only visits from this week" do
+    assert_equal 2, short_urls(:hn).visits_this_week.count
+    assert_equal [visits(:visit_hn_today), visits(:visit_hn_2_days_ago)], short_urls(:hn).visits_this_week
+  end
+
+  test "visits_this_month should return only visits from this month" do
+    assert_equal 3, short_urls(:hn).visits_this_month.count
+    assert_equal [visits(:visit_hn_today), visits(:visit_hn_2_days_ago),visits(:visit_hn_2_weeks_ago)], short_urls(:hn).visits_this_month
   end
 end
