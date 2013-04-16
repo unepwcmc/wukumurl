f674f480e2eca0fb48437eedaecf1cdba3fc24f9
Decio Battaglia <decio.battaglia@gmail.com>
2013-04-16 15:17:36 +0100

    setup compare controller model query

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,7 +1,20 @@
 class CompareController < ApplicationController
   def index
     url_ids = params[:tags].split("/").map { |s| s.to_i }
-    
-    puts url_ids
+    @urls = ShortUrl
+      .where(:id => url_ids)
+      .includes([:visits])
+      .to_json(
+        :only => [:id, :short_name], 
+        :methods => [
+          :visit_count, 
+          :visits_today,
+          :visits_this_week,
+          :visits_this_month,
+          :visits_by_country,
+          :visits_by_organization
+        ]
+      )
+    puts @urls
   end
 end
