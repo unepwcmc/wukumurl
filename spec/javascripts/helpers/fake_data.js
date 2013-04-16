08f917eaa87fadde00483f656acf055dcb1131a2
Decio Battaglia <decio.battaglia@gmail.com>
2013-04-16 15:26:28 +0100

    aded konacha tests

---
+++ spec/javascripts/helpers/fake_data.js
@@ -0,0 +1,358 @@
+[
+  {
+    "id": 108,
+    "short_name": "soerprojectteam",
+    "visit_count": 10,
+    "visits_today": [
+      
+    ],
+    "visits_this_week": [
+      
+    ],
+    "visits_this_month": [
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T10:42:00Z",
+        "id": 725,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T10:45:21Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T10:46:24Z",
+        "id": 726,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T11:15:22Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T10:46:54Z",
+        "id": 727,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T11:15:22Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T10:47:10Z",
+        "id": 728,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T11:15:22Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T10:47:37Z",
+        "id": 729,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T11:15:22Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T10:48:35Z",
+        "id": 730,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T11:15:22Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T11:06:45Z",
+        "id": 731,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T11:15:22Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T11:31:57Z",
+        "id": 732,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T11:42:20Z"
+      },
+      {
+        "country_id": 21,
+        "created_at": "2013-04-07T12:57:42Z",
+        "id": 733,
+        "ip_address": "94.56.161.247",
+        "organization_id": 69,
+        "short_url_id": 108,
+        "updated_at": "2013-04-07T13:09:19Z"
+      },
+      {
+        "country_id": 2,
+        "created_at": "2013-04-08T06:53:13Z",
+        "id": 743,
+        "ip_address": "66.249.76.196",
+        "organization_id": 59,
+        "short_url_id": 108,
+        "updated_at": "2013-04-08T06:54:15Z"
+      }
+    ],
+    "visits_by_country": [
+      {
+        "id": 21,
+        "name": "United Arab Emirates",
+        "visit_count": "9"
+      },
+      {
+        "id": 2,
+        "name": "United States",
+        "visit_count": "1"
+      }
+    ],
+    "visits_by_organization": [
+      {
+        "id": 69,
+        "name": "MUSANADA",
+        "visit_count": "9"
+      },
+      {
+        "id": 59,
+        "name": "Google",
+        "visit_count": "1"
+      }
+    ]
+  },
+  {
+    "id": 106,
+    "short_name": "45ed",
+    "visit_count": 18,
+    "visits_today": [
+      
+    ],
+    "visits_this_week": [
+      
+    ],
+    "visits_this_month": [
+      {
+        "country_id": 1,
+        "created_at": "2013-04-02T14:03:24Z",
+        "id": 494,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-02T14:06:18Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-02T15:42:11Z",
+        "id": 499,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-02T15:45:21Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-02T17:24:32Z",
+        "id": 505,
+        "ip_address": "130.88.39.236",
+        "organization_id": 71,
+        "short_url_id": 106,
+        "updated_at": "2013-04-02T17:45:19Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-03T08:28:41Z",
+        "id": 521,
+        "ip_address": "131.111.27.50",
+        "organization_id": 2,
+        "short_url_id": 106,
+        "updated_at": "2013-04-03T08:33:19Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-03T08:39:22Z",
+        "id": 522,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-03T08:48:20Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-03T09:12:56Z",
+        "id": 524,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-03T09:15:14Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-03T09:50:22Z",
+        "id": 541,
+        "ip_address": "82.20.70.162",
+        "organization_id": 74,
+        "short_url_id": 106,
+        "updated_at": "2013-04-03T09:57:18Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-03T14:54:13Z",
+        "id": 619,
+        "ip_address": "86.167.62.85",
+        "organization_id": 4,
+        "short_url_id": 106,
+        "updated_at": "2013-04-03T15:12:19Z"
+      },
+      {
+        "country_id": 2,
+        "created_at": "2013-04-04T14:32:21Z",
+        "id": 653,
+        "ip_address": "66.249.73.105",
+        "organization_id": 59,
+        "short_url_id": 106,
+        "updated_at": "2013-04-04T14:33:16Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-05T08:05:44Z",
+        "id": 672,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-05T08:15:21Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-05T08:05:52Z",
+        "id": 673,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-05T08:15:21Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-05T08:06:05Z",
+        "id": 674,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-05T08:15:21Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-05T08:17:28Z",
+        "id": 677,
+        "ip_address": "194.59.188.126",
+        "organization_id": 1,
+        "short_url_id": 106,
+        "updated_at": "2013-04-05T08:24:14Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-05T08:46:38Z",
+        "id": 680,
+        "ip_address": "82.1.186.251",
+        "organization_id": 100,
+        "short_url_id": 106,
+        "updated_at": "2013-04-05T08:57:15Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-05T17:35:31Z",
+        "id": 701,
+        "ip_address": "86.6.159.114",
+        "organization_id": 7,
+        "short_url_id": 106,
+        "updated_at": "2013-04-05T17:51:21Z"
+      },
+      {
+        "country_id": 2,
+        "created_at": "2013-04-06T14:16:28Z",
+        "id": 715,
+        "ip_address": "66.249.74.194",
+        "organization_id": 59,
+        "short_url_id": 106,
+        "updated_at": "2013-04-06T14:18:14Z"
+      },
+      {
+        "country_id": 1,
+        "created_at": "2013-04-07T07:42:01Z",
+        "id": 722,
+        "ip_address": "86.162.157.28",
+        "organization_id": 4,
+        "short_url_id": 106,
+        "updated_at": "2013-04-07T07:54:21Z"
+      },
+      {
+        "country_id": 12,
+        "created_at": "2013-04-08T03:57:44Z",
+        "id": 741,
+        "ip_address": "203.201.63.98",
+        "organization_id": 108,
+        "short_url_id": 106,
+        "updated_at": "2013-04-08T04:00:40Z"
+      }
+    ],
+    "visits_by_country": [
+      {
+        "id": 1,
+        "name": "United Kingdom",
+        "visit_count": "15"
+      },
+      {
+        "id": 2,
+        "name": "United States",
+        "visit_count": "2"
+      },
+      {
+        "id": 12,
+        "name": "India",
+        "visit_count": "1"
+      }
+    ],
+    "visits_by_organization": [
+      {
+        "id": 108,
+        "name": "GPS Survey",
+        "visit_count": "1"
+      },
+      {
+        "id": 2,
+        "name": "University of Cambridge",
+        "visit_count": "1"
+      },
+      {
+        "id": 100,
+        "name": "The Institute Of Engineering",
+        "visit_count": "1"
+      },
+      {
+        "id": 1,
+        "name": "World Conservation Monitoring Centre",
+        "visit_count": "8"
+      },
+      {
+        "id": 71,
+        "name": "University of Manchester",
+        "visit_count": "1"
+      },
+      {
+        "id": 74,
+        "name": "Hewitsons Llp",
+        "visit_count": "1"
+      },
+      {
+        "id": 59,
+        "name": "Google",
+        "visit_count": "2"
+      }
+    ]
+  }
+]
