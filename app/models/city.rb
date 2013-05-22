8157260507d7c0ca7a7c2a8ef70ed3f2c4ca6abb
migtorres <miguel.torres@unep-wcmc.org>
2013-05-22 16:09:48 +0100

    Added geocoder to gemfile

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,14 @@
 class City < ActiveRecord::Base
   attr_accessible :country, :iso2, :iso3, :latitude, :longitude, :name, :region
+  has_many :visits
+
+    MaxMindMappings = {country_name: :country, country_code: :iso2, country_code3: :iso3, latitude: :latidude, longitude: :longitude, city: :name, region_name: :region}
+
+  def self.find_or_create_by_max_mind_attributes attributes
+    # Map max mind names to model names
+    attributes = Hash[attributes.map {|k, v| [MaxMindMappings[k], v] }]
+
+    city = City.where(attributes).first || City.create(attributes)
+  end
+
 end
--- app/models/url_location.rb
+++ app/models/url_location.rb
@@ -1,3 +1,18 @@
 class UrlLocation < ActiveRecord::Base
   # attr_accessible :title, :body
+require 'geocoder'
+
+#Uses geocoder to get lat/lon
+  def get_location_from_geocoder(address)
+    latitude = Geocoder::coordinates(@address)[0]
+    longitude = Geocoder::coordinates(@address)[1]
+  end
+
+  def get_location_from_geoip(address)
+    latitude = Geocoder::coordinates(@address)[0]
+    longitude = Geocoder::coordinates(@address)[1]
+  end
+
+
+
 end
