61795692e6a69aadd2e75b91b10490133de0e57d
migtorres <miguel.torres@unep-wcmc.org>
2013-05-29 17:09:20 +0100

    Cities now working

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,10 +5,11 @@ class City < ActiveRecord::Base
   MaxMindMappings = {country_name: :country, country_code: :iso2, country_code3: :iso3, latitude: :latitude, longitude: :longitude, city: :name}
 
   def self.find_or_create_by_max_mind_attributes attributes
-      print new_attributes = attributes.select {:country_name, :country_code, :country_code, :latitude, :longitude, :city}
+      new_attributes = attributes.slice(:country_name,:country_code,:latitude,:longitude,:city)
     # Map max mind names to model names
       new_attributes = Hash[new_attributes.map {|k, v| [MaxMindMappings[k], v] }]
-      City.where(new_attributes).first || City.create(new_attributes)
+      puts new_attributes
+      city = City.where(new_attributes).first || City.create(new_attributes)
   end
 
 end
