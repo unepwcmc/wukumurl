d34ac8b3d06ea418918d960310341a6fc8949524
migtorres <miguel.torres@unep-wcmc.org>
2013-05-29 17:57:24 +0100

    FIxed location bugs

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,16 +3,25 @@ class Location < ActiveRecord::Base
   has_many :visits
 
   def self.get_coordinates_using_geoip city_attributes
-    location = Location.create({ "lat" => city_attributes[:latitude], 
+    location_attributes = { "lat" => city_attributes[:latitude], 
                                  "lon" => city_attributes[:longitude], 
-                                 "source" => "GeoIP" })
+                                 "source" => "GeoIP" }
+    location = Location.where(location_attributes).first || Location.create(location_attributes)
   end
 
   def self.get_coordinates_using_geocoder global_attributes
-    coordinates = Geocoder::coordinates("#{global_attributes[:name]} #{global_attributes[:city]} #{global_attributes[:country_name]}")
-    location = Location.create({ "lat" => global_attributes[:latitude], 
-                                 "lon" => global_attributes[:longitude], 
-                                 "source" => "Geocoder" })
+    geocoder_coordinates = Geocoder::coordinates("#{global_attributes[:name]} #{global_attributes[:city]} #{global_attributes[:country_name]}")
+    if geocoder_coordinates
+      location_attributes = { "lat" => geocoder_coordinates[0], 
+                   "lon" => geocoder_coordinates[1], 
+                   "source" => "Geocoder" }
+      location =  Location.where(location_attributes).first || Location.create(location_attributes)
+    else
+      location_attributes = { "lat" => global_attributes[:latitude], 
+                              "lon" => global_attributes[:longitude], 
+                              "source" => "GeoIP" }
+      location =  Location.where(location_attributes).first || Location.create(location_attributes)
+    end
   end
 end
 
