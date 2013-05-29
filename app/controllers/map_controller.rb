ff3bf0e0f30b6fd5d1df436a3a384d9fc085f578
migtorres <miguel.torres@unep-wcmc.org>
2013-05-29 13:29:02 +0100

    added location to map controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -7,7 +7,8 @@ class MapController < ApplicationController
         :only => [:id, :short_name, :url],
         :methods => [
           :visit_count, 
-          :visits_location
+          :location,
+          :location_id
         ]
       )
       render json: short_urls
