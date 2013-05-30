ef5ec2b6cfb7d900167e457796923f753ddbb513
migtorres <miguel.torres@unep-wcmc.org>
2013-05-30 10:13:15 +0100

    Changes to the way we tore city data

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,10 +5,10 @@ class City < ActiveRecord::Base
   MaxMindMappings = {country_name: :country, country_code: :iso2, country_code3: :iso3, latitude: :latitude, longitude: :longitude, city: :name}
 
   def self.find_or_create_by_max_mind_attributes attributes
-      new_attributes = attributes.slice(:country_name,:country_code,:latitude,:longitude,:city)
+      short_attributes = attributes.slice(:country_name,:country_code,:latitude,:longitude,:city)
     # Map max mind names to model names
-      new_attributes = Hash[new_attributes.map {|k, v| [MaxMindMappings[k], v] }]
-      city = City.where(new_attributes).first || City.create(new_attributes)
+      short_attributes = Hash[short_attributes.map {|k, v| [MaxMindMappings[k], v] }]
+      city = City.where(short_attributes).first || City.create(short_attributes)
   end
 
 end
