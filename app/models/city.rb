456ec65f44b64764a0728c1ff09c00acc33b7e69
migtorres <miguel.torres@unep-wcmc.org>
2013-05-30 10:59:53 +0100

    Add city to map controller

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,11 +1,11 @@
 class City < ActiveRecord::Base
-  attr_accessible :country, :iso2, :iso3, :latitude, :longitude, :name, :region
+  attr_accessible :country, :iso2, :iso3, :lat, :lon, :name, :region
   has_many :visits
 
-  MaxMindMappings = {country_name: :country, country_code: :iso2, country_code3: :iso3, latitude: :latitude, longitude: :longitude, city: :name}
+  MaxMindMappings = {country_name: :country, country_code: :iso2, country_code3: :iso3, latitude: :lat, longitude: :lon, city: :name, region: :region}
 
   def self.find_or_create_by_max_mind_attributes attributes
-      short_attributes = attributes.slice(:country_name,:country_code,:latitude,:longitude,:city)
+      short_attributes = attributes.slice(:country_name, :country_code, :latitude, :longitude, :region, :city)
     # Map max mind names to model names
       short_attributes = Hash[short_attributes.map {|k, v| [MaxMindMappings[k], v] }]
       city = City.where(short_attributes).first || City.create(short_attributes)
