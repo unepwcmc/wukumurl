f4e0841b11304055f44d0299c5ae19c7f80fbc1e
Adam Mulligan <adammulligan@gmail.com>
2013-10-15 12:06:38 +0100

    Use new Locations correctly

--- test/unit/visit_test.rb
+++ test/unit/visit_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class VisitTest < ActiveSupport::TestCase
-  test "Geolocate on a wcmc IP visit should set country and organization correctly" do
+  test "Geolocate on a wcmc IP visit should set organization correctly" do
     wcmc_visit = visits(:wcmc_visit_hn_today)
     assert_nil wcmc_visit.country
 
@@ -10,12 +10,14 @@ class VisitTest < ActiveSupport::TestCase
     odb = GeoIP::Organization.new(GEO_IP_CONFIG['org_db'])
 
     wcmc_visit.geo_locate cdb, odb
-    assert_equal "GB", wcmc_visit.country.iso
     assert_equal "World Conservation Monitoring Centre", wcmc_visit.organization.name
   end
 
   test "Visit.un_geolocated should return only visits which have no country set" do
-    assert Visit.un_geolocated.to_a.include?(visits(:wcmc_visit_hn_today))
-    assert !Visit.un_geolocated.to_a.include?(visits(:old_canadian_geolocated))
+    assert Visit.un_geolocated.to_a.include?(visits(:wcmc_visit_hn_today)),
+      "Expected ungeolocated Visits to contain HN visit"
+
+    assert !Visit.un_geolocated.to_a.include?(visits(:old_canadian_geolocated)),
+      "Expected ungeolocated to not contain old_canadian visit"
   end
 end
