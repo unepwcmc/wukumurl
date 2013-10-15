8e6344bc8a8198cd41138d77bb343f51504b5b7c
Adam Mulligan <adammulligan@gmail.com>
2013-10-15 11:31:38 +0100

    Can append /info to URL to get info page

--- config/routes.rb
+++ config/routes.rb
@@ -11,68 +11,16 @@ Wukumurl::Application.routes.draw do
   match '/' => 'short_urls#create', :controller => 'short_urls', :via => :post
 
   match "/:short_name", :controller => 'short_urls', :action => :visit, :as => 'visit_short_url'
+  match "/:short_name/info",
+    :controller => 'short_urls',
+    :action => :show,
+    :as => 'short_url_info'
 
-  #match '/compare/urls/:link1/:link2', :controller => 'compare', :action => :show_urls, :via => :get
   match '/compare/*tags' => "compare#index"
 
   resources :short_urls
 
   resources :organizations, only: [:destroy]
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'short_urls#visit'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
--- test/functional/short_urls_controller_test.rb
+++ test/functional/short_urls_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class ShortUrlsControllerTest < ActionController::TestCase
+  test "can access a ShortUrl info page by appending /info to the link" do
+    short_url = short_urls(:hn)
+    get :show, short_name: short_url.short_name
+    assert :success
+  end
+
+  test "GET show redirects to home page if ShortUrl doesn't exist" do
+    get :show, short_name: "GOB"
+    assert_redirected_to :index
+  end
+
   test "should add URLs if non_robot parameter is true" do
     post :create, url: "http://envirobear.com", not_a_robot: true
     assert :success
