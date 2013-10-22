93aa9ba488837aca788589ea886630542e2703fb
Adam Mulligan <adammulligan@gmail.com>
2013-10-22 20:27:30 +0100

    Rails 4 routes must specify the HTTP request type
    http://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#upgrading-from-rails-3-2-to-rails-4-0-gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Wukumurl::Application.routes.draw do
   get "map", :controller => 'map', :action => :index
-  match "map/locations", :controller => 'map', :action => :location_list
-  match "map/cities", :controller => 'map', :action => :city_list
-  match "map/countries", :controller => 'map', :action => :country_list
+  get "map/locations", :controller => 'map', :action => :location_list
+  get "map/cities", :controller => 'map', :action => :city_list
+  get "map/countries", :controller => 'map', :action => :country_list
 
   get "compare/index"
 
-  match '/help' => 'help#index'
+  get '/help' => 'help#index'
 
   match '/' => 'short_urls#create', :controller => 'short_urls', :via => :post
 
-  match "/:short_name", :controller => 'short_urls', :action => :visit, :as => 'visit_short_url'
-  match "/:short_name/info",
+  get "/:short_name", :controller => 'short_urls', :action => :visit, :as => 'visit_short_url'
+  get "/:short_name/info",
     :controller => 'short_urls',
     :action => :show,
     :as => 'short_url_info'
 
-  match '/compare/*tags' => "compare#index"
+  get '/compare/*tags' => "compare#index"
 
   resources :short_urls
 
