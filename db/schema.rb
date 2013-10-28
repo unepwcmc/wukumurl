9aed07a253d473c99544669ae55d5bd9bd0e4ca8
Adam Mulligan <adammulligan@gmail.com>
2013-10-28 21:28:16 +0000

    Update Visit test

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131023070734) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: true do |t|
     t.string   "iso2"
     t.string   "iso3"
--- test/unit/visit_test.rb
+++ test/unit/visit_test.rb
@@ -1,16 +1,35 @@
 require 'test_helper'
 
 class VisitTest < ActiveSupport::TestCase
+  def setup
+    require 'geoip'
+    @country_db = GeoIP::Country.new(GEO_IP_CONFIG['country_db'])
+    @organisation_db = GeoIP::Organization.new(GEO_IP_CONFIG['org_db'])
+  end
+
   test "Geolocate on a wcmc IP visit should set organization correctly" do
     wcmc_visit = visits(:wcmc_visit_hn_today)
-    assert_nil wcmc_visit.country
+    assert_nil wcmc_visit.organization
 
-    require 'geoip'
-    cdb = GeoIP::Country.new(GEO_IP_CONFIG['country_db'])
-    odb = GeoIP::Organization.new(GEO_IP_CONFIG['org_db'])
-
-    wcmc_visit.geo_locate cdb, odb
+    wcmc_visit.geo_locate @country_db, @organisation_db
     assert_equal "World Conservation Monitoring Centre", wcmc_visit.organization.name
+  end
+
+  test "Geolocate on a wcmc IP visit should set city correctly" do
+    wcmc_visit = visits(:wcmc_visit_hn_today)
+    assert_nil wcmc_visit.city
+
+    wcmc_visit.geo_locate @country_db, @organisation_db
+    assert_equal "Cambridge", wcmc_visit.city.name
+  end
+
+  test "Geolocate on a wcmc IP visit should set location correctly" do
+    wcmc_visit = visits(:wcmc_visit_hn_today)
+    assert_nil wcmc_visit.location
+
+    wcmc_visit.geo_locate @country_db, @organisation_db
+    assert_equal 52.22, wcmc_visit.location.lat.round(2)
+    assert_equal 0.1, wcmc_visit.location.lon.round(2)
   end
 
   test "Visit.un_geolocated should return only visits which have no country set" do
