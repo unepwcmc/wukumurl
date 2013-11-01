484b709396afaf08229a844fd616ff1e352d7924
Adam Mulligan <adammulligan@gmail.com>
2013-11-01 08:57:08 +0000

    White spaaaaaaaaaaaaaace

--- app/models/country_location.rb
+++ app/models/country_location.rb
@@ -3,6 +3,6 @@ class CountryLocation < ActiveRecord::Base
   has_many :short_urls, :through => :city
 
   def country_urls
-   CountryLocation.where(iso2: self.iso2).joins(:short_urls).select([:short_name, :url, :country]).where("cities.iso2 IS NOT NULL")
+    CountryLocation.where(iso2: self.iso2).joins(:short_urls).select([:short_name, :url, :country]).where("cities.iso2 IS NOT NULL")
   end
 end
