bc49f7c138040d92b4863b9b996107ac4c233307
Adam Mulligan <adammulligan@gmail.com>
2013-11-04 15:20:57 +0000

    Remove disregard column from orgs (replaced by DisregardVote)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131104145719) do
+ActiveRecord::Schema.define(version: 20131104152033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,9 +54,8 @@ ActiveRecord::Schema.define(version: 20131104145719) do
 
   create_table "organizations", force: true do |t|
     t.text     "name"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "disregard",  default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "short_urls", force: true do |t|
