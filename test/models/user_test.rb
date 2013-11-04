2b771c98b3d714407fcce283f96adc90fb3c3043
Adam Mulligan <adammulligan@gmail.com>
2013-11-04 11:09:11 +0000

    Test User ShortUrl associations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create a User with multiple ShortUrl associations" do
+    short_urls = [
+      FactoryGirl.create(:short_url),
+      FactoryGirl.create(:short_url)
+    ]
+
+    user = FactoryGirl.create(:user, short_urls: short_urls)
+
+    assert_equal short_urls, user.short_urls
+  end
 end
