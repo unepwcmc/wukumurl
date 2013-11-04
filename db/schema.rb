5ed263ee7fcde1b0b5bd61d7d38603853f3bf743
Adam Mulligan <adammulligan@gmail.com>
2013-11-04 08:29:30 +0000

    Relate short urls to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029174213) do
+ActiveRecord::Schema.define(version: 20131104080255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20131029174213) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean  "deleted"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
