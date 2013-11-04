3c3de14ef3b0638a89b57aac418a28f5cccf6a64
Adam Mulligan <adammulligan@gmail.com>
2013-11-04 15:19:31 +0000

    Deleting organisations creates a DisregardVote instance

--- app/models/disregard_vote.rb
+++ app/models/disregard_vote.rb
@@ -1,2 +1,4 @@
 class DisregardVote < ActiveRecord::Base
+  belongs_to :organization
+  belongs_to :short_url
 end
--- test/models/disregard_vote_test.rb
+++ test/models/disregard_vote_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DisregardVoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create a DisregardVote model with Organization and ShortUrl associations" do
+    short_url = FactoryGirl.create(:short_url)
+    organization = FactoryGirl.create(:organization)
+
+    disregard_vote = FactoryGirl.create(:disregard_vote,
+      organization: organization, short_url: short_url)
+
+    assert_equal organization, disregard_vote.organization
+    assert_equal short_url, disregard_vote.short_url
+  end
 end
