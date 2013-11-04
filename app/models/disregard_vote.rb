436df503e6fa9c871efb775b5e29cde12be3159e
Adam Mulligan <adammulligan@gmail.com>
2013-11-04 14:58:11 +0000

    DisregardVote model

---
+++ app/models/disregard_vote.rb
@@ -0,0 +1,2 @@
+class DisregardVote < ActiveRecord::Base
+end
---
+++ test/models/disregard_vote_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DisregardVoteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
