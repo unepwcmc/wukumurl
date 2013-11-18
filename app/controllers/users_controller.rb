9861f05d09eb264ef0824fc2ba71683df95d112b
Adam Mulligan <adammulligan@gmail.com>
2013-11-18 15:47:25 +0000

    Get user's visit counts grouped by country

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @visits_by_country = City.
-      select("country, count(*) as value").
-      joins(:visits).
-      group(:country).
-      order('value desc')
-
+    @visits_by_country = current_user.visits_by_country
     @visits_by_organization = current_user.visits_by_organization
 
     @total_visits = current_user.visits.length
