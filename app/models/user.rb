87c67ed54f8d9517c61f05c80d4897b950ae8e8b
Adam Mulligan <adammulligan@gmail.com>
2013-11-18 15:04:13 +0000

    Get user's visits by organization

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,29 @@ class User < ActiveRecord::Base
     ")
   end
 
+  def visits_by_organization
+    Organization.find_by_sql("
+      SELECT organizations.*, visit_count
+      FROM organizations
+      INNER JOIN
+        (
+          SELECT
+            COUNT(visits.id) as visit_count, visits.organization_id, visits.short_url_id
+          FROM visits
+          GROUP BY organization_id, short_url_id
+        ) AS visits_for_orgs
+      ON visits_for_orgs.organization_id = organizations.id
+      INNER JOIN
+        (
+          SELECT short_urls.id
+          FROM short_urls
+          WHERE user_id = #{self.id}
+          GROUP BY id
+        ) AS short_urls_for_visits
+      ON visits_for_orgs.short_url_id = short_urls_for_visits.id
+    ")
+  end
+
   def no_urls?
     self.short_urls.length == 0
   end
