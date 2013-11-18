a85ec7d9ebfe0c20d49422e8841d8750f892c08e
Adam Mulligan <adammulligan@gmail.com>
2013-11-18 11:01:37 +0000

    Improve readability of code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def show
-    @visits_by_country = City.select("country, count(*) as value")
-      .joins(:visits).group(:country).order('value desc')
-    @visits_by_organization = Organization.select("name, count(*) as value")
-      .joins(:visits).group(:name).order('value desc')
+    @visits_by_country = City.
+      select("country, count(*) as value").
+      joins(:visits).
+      group(:country).
+      order('value desc')
+
+    @visits_by_organization = Organization.
+      select("name, count(*) as value").
+      joins(:visits).
+      group(:name).
+      order('value desc')
 
     @total_visits = Visit.count
     @total_urls   = ShortUrl.count
