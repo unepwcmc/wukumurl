74874aaa39513b2a14d8c00bf0ed8174de69a6be
Adam Mulligan <adammulligan@gmail.com>
2013-11-20 13:57:11 +0000

    Move queries from controllers to model

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,10 +6,17 @@ class City < ActiveRecord::Base
   MaxMindMappings = {country_name: :country, country_code: :iso2, country_code3: :iso3, latitude: :lat, longitude: :lon, city: :name, region: :region}
 
   def self.find_or_create_by_max_mind_attributes attributes
-      short_attributes = attributes.slice(:country_name, :country_code, :latitude, :longitude, :region, :city)
+    short_attributes = attributes.slice(:country_name, :country_code, :latitude, :longitude, :region, :city)
     # Map max mind names to model names
-      short_attributes = Hash[short_attributes.map {|k, v| [MaxMindMappings[k], v] }]
-      city = City.where(short_attributes).first || City.create(short_attributes)
+    short_attributes = Hash[short_attributes.map {|k, v| [MaxMindMappings[k], v] }]
+    City.where(short_attributes).first || City.create(short_attributes)
+  end
+
+  def self.all_visits_by_country
+    select("country, count(*) as value").
+      joins(:visits).
+      group(:country).
+      order('value desc')
   end
 
   def city_urls
