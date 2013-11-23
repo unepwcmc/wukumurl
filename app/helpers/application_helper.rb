43ffb5f4e56c9f69d674935f4fdebe8e2e58adca
Adam Mulligan <adammulligan@gmail.com>
2013-11-23 17:43:28 +0000

    Only show title back button where appropriate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 module ApplicationHelper
+  def back_button
+    if controller.controller_name == 'short_urls' &&
+      controller.action_name == 'show'
+      return link_to(
+        '<i class="fa fa-chevron-circle-left"></i>'.html_safe,
+        "/",
+        class: 'back'
+      )
+    end
+  end
+
   def title page_title
     content_for :title, page_title.to_s
   end
