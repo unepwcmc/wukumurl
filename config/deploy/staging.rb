b05b30614d1c3958fe9ae672be967ddd2b153b0b
Adam Mulligan <adammulligan@gmail.com>
2013-11-28 15:36:44 +0000

    Remove legacy rvm capistrano config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,11 +10,6 @@ set :app_port, "80"
 
 set :rvm_ruby_string, '2.0.0'
 
-# Add RVM's lib directory to the load path.
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-
-set :rvm_type, :user  # Literal ":user"
-
 # Load RVM's capistrano plugin.
 require 'rvm/capistrano'
 
