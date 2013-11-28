3e6f63212ad0b7af8b81964abd3ffe46953d9aa2
Adam Mulligan <adammulligan@gmail.com>
2013-11-28 15:34:09 +0000

    Show sign in box if sign in failed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < Devise::SessionsController
   layout false
+
+  def create
+    resource = warden.authenticate!(
+      :scope => resource_name,
+      :recall => "#{controller_path}#failure"
+    )
+
+    sign_in_and_redirect(resource_name, resource)
+  end
+
+  def sign_in_and_redirect(resource_or_scope, resource=nil)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    resource ||= resource_or_scope
+    sign_in(scope, resource) unless warden.user(scope) == resource
+    redirect_to :back
+  end
+
+  def failure
+    flash[:errors] ||= {}
+    flash[:errors][:user] = ["Incorrect username or password"]
+    return redirect_to :back
+  end
 end
