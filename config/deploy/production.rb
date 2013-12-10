dc89970437dc8e8a5a3b67752ce0889268dd9636
Adam Mulligan <adammulligan@gmail.com>
2013-12-10 13:55:55 +0000

    Deployment config for new brightbox servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,68 @@
 set :rails_env, "production"
-# Primary domain name of your application. Used in the Apache configs
-set :domain, "unepwcmc-004.vm.brightbox.net"
-## List of servers
-server "unepwcmc-004.vm.brightbox.net", :app, :web, :db, :primary => true
+set :domain, "unepwcmc-014.vm.brightbox.net"
+
+server "unepwcmc-014.vm.brightbox.net", :app, :web, :db, :primary => true
+
+set :application, "wukumurl"
+set :server_name, "wukumurl.unepwcmc-014.vm.brightbox.net"
+set :sudo_user, "rails"
+set :app_port, "80"
+
+set :rvm_ruby_string, '2.0.0'
+
+# Load RVM's capistrano plugin.
+require 'rvm/capistrano'
+
+desc "Configure VHost"
+task :config_vhost do
+  vhost_config =<<-EOF
+    server {
+      listen 80;
+      client_max_body_size 4G;
+      server_name #{application}.unepwcmc-014.vm.brightbox.net #{application}.sw02.matx.info;
+      keepalive_timeout 5;
+      root #{deploy_to}/current/public;
+      passenger_enabled on;
+      rails_env production;
+
+      add_header 'Access-Control-Allow-Origin' *;
+      add_header 'Access-Control-Allow-Methods' "GET, POST, PUT, DELETE, OPTIONS";
+      add_header 'Access-Control-Allow-Headers' "X-Requested-With, X-Prototype-Version";
+      add_header 'Access-Control-Max-Age' 1728000;
+
+      gzip on;
+      location ^~ /assets/ {
+        expires max;
+        add_header Cache-Control public;
+      }
+
+      if (-f $document_root/system/maintenance.html) {
+        return 503;
+      }
+
+      error_page 500 502 504 /500.html;
+      location = /500.html {
+        root #{deploy_to}/public;
+      }
+
+      error_page 503 @maintenance;
+      location @maintenance {
+        rewrite  ^(.*)$  /system/maintenance.html break;
+      }
+    }
+  EOF
+
+  put vhost_config, "/tmp/vhost_config"
+
+  sudo "mv /tmp/vhost_config /etc/nginx/sites-available/#{application}"
+  sudo "ln -s /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}"
+end
+
+after "deploy:setup", :config_vhost
+
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
