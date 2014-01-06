4e17374f24aca98351a88e05eb4283e94d3c16ac
Adam Mulligan <adammulligan@gmail.com>
2014-01-06 10:46:32 +0000

    Display a short url's actual link next to its ID on the dashboard

--- app/helpers/short_urls_helper.rb
+++ app/helpers/short_urls_helper.rb
@@ -2,4 +2,10 @@ module ShortUrlsHelper
   def recently_added short_url
     return short_url.created_at > 1.minute.ago
   end
+
+  def pretty_url url
+    url.gsub!(/https{0,1}:\/\//, '')
+    url.gsub!(/^www\./, '')
+    return url
+  end
 end
