c6303c163a280f7b78509ea9b3a007f007c8758a
Adam Mulligan <adammulligan@gmail.com>
2014-02-13 10:09:02 +0000

    Don't include secret key in repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,11 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wukumurl::Application.config.secret_key_base = '2131530FEC3B41ADF8B5421E8745528A34603993568C88EEA810BB8DE7487501'
+
+if Rails.env.development? or Rails.env.test?
+  token = ('x' * 30)
+else
+  token = ENV['SECRET_TOKEN']
+end
+
+Wukumurl::Application.config.secret_key_base = token
