07cb96027ae38de877309ac152a27f01f962c56f
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-04-16 15:58:08 +0100

    added ZeroClipboard to precompilation chain in staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -48,7 +48,7 @@ Wukumurl::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( ZeroClipboard.swf )
 
   config.serve_static_assets = true
 
