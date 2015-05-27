266aa7e4ae2a12e4a1493d41d07d5d6e0a2cd0b9
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-05-27 14:33:31 +0100

    deploy to staging from master

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,6 @@ set :rails_env, "staging"
 
 set :domain, "unepwcmc-011.vm.brightbox.net"
 server "unepwcmc-011.vm.brightbox.net", :app, :web, :db, :primary => true
-set :branch, "develop"
 set :application, "wukumurl"
 set :server_name, "wukumurl.unepwcmc-011.vm.brightbox.net"
 set :sudo_user, "rails"
