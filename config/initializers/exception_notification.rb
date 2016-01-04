b3b38d6b13e407ae605adb020ecc2fd6fd5f86bd
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-01-04 10:52:19 +0000

    Minor design tweaks

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,13 +1,13 @@
 require 'exception_notification/rails'
 
-ExceptionNotification.configure do |config|
-  config.ignore_if do |exception, options|
-    not (Rails.env.production? || Rails.env.staging?)
-  end
+#ExceptionNotification.configure do |config|
+  #config.ignore_if do |exception, options|
+    #not (Rails.env.production? || Rails.env.staging?)
+  #end
 
   #config.add_notifier :slack, {
     #webhook_url: "#{ENV[SLACK_NOTIFICATION_WEBHOOK_URL]}",
     #channel: "#wcmc_io",
     #username: "Exception Notification (#{Rails.env})"
   #}
-end
+#end
