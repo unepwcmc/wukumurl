bcca6078a93bd466904d935801dff6f54011802c
Stuart Watson <stuart.watson@unep-wcmc.org>
2016-02-15 13:09:06 +0000

    Added production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,17 @@
+set :stage, :production
+set :branch, "redesign"
+
+
+server "unep-wcmc-production.linode.unep-wcmc.org", user: 'wcmc', roles: %w{app web db}
+
+set :application, "wukumurl"
+set :server_name, "wukumurl.unepwcmc.unep-wcmc-staging.linode.unep-wcmc.org"
+set :sudo_user, "wcmc"
+set :app_port, "80"
+
+
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
